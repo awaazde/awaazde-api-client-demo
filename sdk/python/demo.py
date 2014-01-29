eeb50c164f156457bd0897878cc4f35b454ed165
Nikhil Navadiya <nikhil.navadiya@gmail.com>
2014-01-28 23:31:15 -0800

    Added callMgr in python sdk

--- sdk/python/demo.py
+++ sdk/python/demo.py
@@ -8,6 +8,7 @@
 
 
 from xactclient.data.datamgr import TemplateMgr
+from xactclient.data.datamgr import CallMgr
 from xactclient.common.authdata import AuthData
 
 USERNAME = 'your username'
@@ -19,11 +20,31 @@ def main():
     
     templateMgr = TemplateMgr(authdata)
     
-    print 'Getting all templates'
-    print templateMgr.getAll()
+    #print 'Getting all templates'
+    #print templateMgr.getAll()
     
-    print 'Getting template data with id 1'
-    print templateMgr.getTemplate('1')
+    #print 'Getting template data with id 1'
+    #print templateMgr.getTemplate('1')
+    
+    callMgr = CallMgr(authdata);
+    #print "Getting all calls"
+    #print callMgr.getAll()
+    
+    #print "Getting call with id 1"
+    #print callMgr.getCall("1")
+    
+    #creating new call
+    data  = {"recipient":"0123456789", "text":"You have 99 elephants waiting at awaaz","send_on":"2014-01-29T14:32:00"}
+    #print "Creating a call with data " + str(data)
+    #print callMgr.create(data)
+    
+    #updating call
+    #data  = {"recipient":"9904602242", "text":"You have 99 elephants waiting at awaaz","send_on":"2014-01-29T14:32:00"}
+    #print "Updating a call with data " + str(data)
+    #print callMgr.update("5007", data)
+    
+    print "deleting a call"
+    print callMgr.delete("5007")
     
 if __name__ =='__main__':main()
 
--- sdk/python/xactclient/data/datamgr.py
+++ sdk/python/xactclient/data/datamgr.py
@@ -6,6 +6,7 @@
 #
 #===============================================================================
 import requests
+import simplejson as json
 
 TEMPLATE_WS_URL = '/template'
 CALL_WS_URL = '/call'
@@ -22,23 +23,110 @@ class TemplateMgr:
     '''
     def getAll(self):
         response = requests.get(self.authdata.getUrl() + TEMPLATE_WS_URL, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
-        assert response.status_code == 200
-        templates_data = []
-        index = 0;
-        for template in response.json():
-            templates_data.insert(index, template)
-            index = index + 1
-        
-        return templates_data
+        if response.status_code == 200:
+            templates_data = []
+            index = 0;
+            for template in response.json():
+                templates_data.insert(index, template)
+                index = index + 1
+            
+            return templates_data
+        else:
+            return response.text
     
     '''
     returns template detail for given template id
     '''
     def getTemplate(self, id):
         response = requests.get(self.authdata.getUrl() + TEMPLATE_WS_URL + '/' + id, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
-        assert response.status_code == 200
-        template = None
-        if response.json():
-            template = response.json()
+        if response.status_code == 200:
+            template = None
+            if response.json():
+                template = response.json()
+                
+            return template
+        else:
+            return response.text
+    
+
+class CallMgr:
+    '''
+    Call Manager is responsible for dealing with call data
+    '''
+    def __init__(self, authdata):
+        self.authdata = authdata
+    
+    '''
+    returns all calls
+    '''
+    def getAll(self):
+        response = requests.get(self.authdata.getUrl() + CALL_WS_URL, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
+        if response.status_code == 200:
+            calls_data = []
+            index = 0;
+            for call in response.json():
+                calls_data.insert(index, call)
+                index = index + 1
             
-        return template
+            return calls_data
+        else:
+            return response.text
+    
+    '''
+    returns call detail for given call id
+    '''
+    def getCall(self, id):
+        response = requests.get(self.authdata.getUrl() + CALL_WS_URL + '/' + id, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
+        if response.status_code == 200:
+            call = None
+            if response.json():
+                call = response.json()
+                
+            return call
+        else:
+            return response.text
+        
+    
+    '''
+    creating new call
+    '''
+    def create(self, calldata):
+        headers = {'content-type': 'application/json'}
+        response = requests.post(self.authdata.getUrl() + CALL_WS_URL + "/", data=json.dumps(calldata), headers=headers, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
+        if response.status_code == 201:
+            call = None
+            if response.json():
+                call = response.json()
+                
+            return call
+        else:
+            return response.text
+        
+    
+    '''
+    editing call
+    '''
+    def update(self, id, calldata):
+        headers = {'content-type': 'application/json'}
+        response = requests.put(self.authdata.getUrl() + CALL_WS_URL + '/' + id, data=json.dumps(calldata), headers=headers, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
+        if response.status_code == 201:
+            call = None
+            if response.json():
+                call = response.json()
+                
+            return call
+        else:
+            return response.text
+        
+        
+    '''
+    deleting call
+    '''
+    def delete(self, id):
+        headers = {'content-type': 'application/json'}
+        response = requests.delete(self.authdata.getUrl() + CALL_WS_URL + '/' + id, headers=headers, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
+        if response.status_code == 204:
+            return "true"
+        else:
+            return response.text
+        
