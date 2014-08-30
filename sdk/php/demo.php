92e8deb457966fabf9630a563c21713ed3e22f5d
Nikhil N <nikhiln@users.noreply.github.com>
2014-08-30 13:15:06 +0530

    Updated web service base url

--- sdk/php/demo.php
+++ sdk/php/demo.php
@@ -10,9 +10,9 @@
 require_once 'include_all.php';
 
  // Xact auth info
-$user 		= "guest";
-$password 	= "guest4all";
-$ws_url		= "http://awaaz.de/console/xact/";
+$user 		= "<your  username>";
+$password 	= "<password>";
+$ws_url		= "https://awaaz.de/console/xact/";
 
 //creating a authdata object
 $authdata = new AuthData($user, $password, $ws_url);
@@ -24,11 +24,11 @@ echo "<h3>Getting all call information</h3>";
 $callMgr = new CallManager($authdata);
 
 //displaying call info
-//html_show_array($callMgr->getAll());
+html_show_array($callMgr->getAll());
 
 //getting specific call info
 echo "<h3>Getting specific call data</h3>";
-html_show_array($callMgr->get('10'));
+html_show_array($callMgr->get('7032'));
 
 //creating new call
 echo "<h3>Creating new call</h3>";
@@ -37,7 +37,7 @@ echo "<h3>Creating new call</h3>";
 $calldata = array(
     'recipient' => '0123456789',
     'text' => 'You have 99 elephants waiting at awaaz',
-    'send_on' => '2013-11-30T14:32:00'
+    'send_on' => '2014-08-30T13:10:00'
 	);
 	
 html_show_array($callMgr->create($calldata));
@@ -47,11 +47,11 @@ html_show_array($callMgr->create($calldata));
 echo "<h3>Editing new call</h3>";
 
 //call data
-$callId = '38';
+$callId = '7032';
 $calldata = array(
     'recipient' => '0123456789',
     'text' => 'You have 33 elephants waiting at awaaz',
-    'send_on' => '2013-12-01T14:32:00'
+    'send_on' => '2014-08-30T13:11:00'
 	);
 	
 html_show_array($callMgr->modify($callId, $calldata));
@@ -61,7 +61,7 @@ html_show_array($callMgr->modify($callId, $calldata));
 echo "<h3>Deleting new call</h3>";
 
 //call data
-$callId = '38';
+$callId = '7032';
 echo 'deleting call with id' . $callId;
 	
 html_show_array($callMgr->delete($callId));
