07de1a881b8387cc7e938f25499d5394e2212ac0
Nikhil Navadiya <nikhil@awaaz.de>
2016-01-19 17:43:53 +0530

    Added stream client

---
+++ sdk/python/demo_stream.py
@@ -0,0 +1,48 @@
+#===============================================================================
+#    Copyright (c) 2014 Awaaz.De
+#    Sample SDK to call awaaz.de xact api in python
+#
+#    @author Nikhil (nikhil@awaaz.de, nikhil.navadiya@gmail.com)
+#
+#===============================================================================
+
+
+from xactclient.data.datamgr import TemplateMgr
+from xactclient.data.datamgr import CallMgr
+from xactclient.common.authdata import AuthData
+
+from streamclient.data.datamgr import GroupMgr
+
+USERNAME = 'nikhil@awaaz.de'
+PASSWORD = 'welcome'
+#WS_URL = 'http://127.0.0.1:8000/console/xact'
+WS_URL = 'http://127.0.0.1:8000/console'
+
+def main():
+    authdata= AuthData(USERNAME,PASSWORD,WS_URL)
+    
+    # stream
+    groupMgr = GroupMgr(authdata)
+    
+    group_data = {'name': 'test1', 'language': 'eng', 'backup_calls': 2, 'touchtone_max_digits': 0}
+    group = groupMgr.create(group_data)
+
+    # update
+
+    group_id = group['id']
+
+    update_group_data = {
+        "name": "test2", 
+        "language": "hin", 
+        "backup_calls": 2, 
+        "touchtone_max_digits": 4
+    }
+    
+    print groupMgr.update(group_id, update_group_data)
+
+    # # delete
+    print groupMgr.delete(group_id)
+
+    
+if __name__ =='__main__':main()
+
---
+++ sdk/python/streamclient/data/datamgr.py
@@ -0,0 +1,79 @@
+#===============================================================================
+#    Copyright (c) 2014 Awaaz.De
+#    This file is part of awaaz.de xact api client lib
+#
+#    @author Nikhil (nikhil@awaaz.de, nikhil.navadiya@gmail.com)
+#
+#===============================================================================
+import requests
+import simplejson as json
+
+GROUP_WS_URL = '/streams-api/groups'
+
+class GroupMgr:
+    '''
+    GroupMgr Manager is responsible for dealing with groups data
+    '''
+    def __init__(self, authdata):
+        self.authdata = authdata
+    
+    '''
+    returns all templates
+    '''
+    def getAll(self):
+        response = requests.get(self.authdata.getUrl() + GROUP_WS_URL, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
+        if response.status_code == 200:
+            templates_data = []
+            for template in response.json()['results']:
+                templates_data.append(template)
+            
+            return templates_data
+        else:
+            return response.text
+    
+    '''
+    returns template detail for given template id
+    '''
+    def get(self, id):
+        response = requests.get(self.authdata.getUrl() + GROUP_WS_URL + '/' + id, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
+        if response.status_code == 200:
+            template = None
+            if response.json():
+                template = response.json()
+                
+            return template
+        else:
+            return response.text
+    
+    def create(self, groupdata):
+        headers = {'content-type': 'application/json'}
+        response = requests.post(self.authdata.getUrl() + GROUP_WS_URL + "/", data=json.dumps(groupdata), headers=headers, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
+        if response.status_code == 201:
+            group = None
+            if response.json():
+                group = response.json()
+             
+            return group
+        else:
+            return response.text
+        
+    def update(self, id, groupdata):
+        headers = {'content-type': 'application/json'}
+        response = requests.put(self.authdata.getUrl() + GROUP_WS_URL + '/' + str(id) + "/", data=json.dumps(groupdata), headers=headers, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
+        if response.status_code == 201:
+            group = None
+            if response.json():
+                group = response.json()
+                
+            return group
+        else:
+            return response.text
+
+    def delete(self, id):
+        headers = {'content-type': 'application/json'}
+        response = requests.delete(self.authdata.getUrl() + GROUP_WS_URL + '/' + str(id) + "/", headers=headers, auth=(self.authdata.getUsername(), self.authdata.getPassword()))
+        if response.status_code == 204:
+            return "true"
+        else:
+            return response.text
+        
